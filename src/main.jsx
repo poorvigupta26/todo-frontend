20886536c51f3cad8118aa848302c4d924254e02
poorvigupta26 <poorvi26mar@gmail.com>
2025-07-05 20:16:32 +0530

    updated main

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,8 @@ import { createContext, StrictMode, useState } from 'react'
 import { createRoot } from 'react-dom/client'
 import {createBrowserRouter, RouterProvider} from "react-router";
 import App from './App.jsx'
-import "./styles/App.scss";
 import Home from './pages/Home.jsx';
 import Profile from './pages/Profile.jsx';
-import path from 'path';
 import Login from './pages/Login.jsx';
 import Register from './pages/Register.jsx';
 
